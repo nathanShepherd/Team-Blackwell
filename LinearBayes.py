d5297c20d1f89b47c4f5e7fc1dbb130285dc2188
Nathan Shepherd <nateshep@umich.edu>
2022-04-06 21:11:42 -0400

    Update

--- LinearBayes.py
+++ LinearBayes.py
@@ -26,9 +26,11 @@ class LinearBayesAgent:
         self.bucket = self.init_bucket(rng, num_bins) 
 
         self.Q = self.init_Q() # Matrix of state-value pairs by action
-        self.lr = 0.1 # Learning Rate
-        self.gamma = 0.9 # Discount factor for future reward
+        self.lr = 0.5 # Learning Rate
+        self.gamma = 0.5 # Discount factor for future reward
         self.epsilon = 0.7 # Epsilon Greedy parameter (annealing)
+
+        self.num_episodes = 0
         
 
     def play_episode(self, train=True, viz=False):
@@ -42,18 +44,24 @@ class LinearBayesAgent:
             act = self.get_action(obs[-1], train)
 
             new_state, reward, terminal, info = env.step(act)
-            self.update_policy(obs[-1], new_state, act, reward)
             
-            actions.append(act)
-            obs.append(new_state)
+            
             total_reward += reward
 
+            self.update_policy(obs[-1], new_state, act, total_reward)
+            actions.append(act)
+            obs.append(new_state)
+
             if terminal:
-                # Attenuate random action prob
-                #self.epsilon *= 0.9
                 # Set Q-Value for terminal state as total reward
                 #_, Q_idx = self.get_Q(obs[-1], act)
                 #self.Q[Q_idx][act] = total_reward
+                #self.full_update(obs, actions, total_reward)
+
+                # Attenuate random action prob
+                #self.num_episodes += 1
+                #if self.num_episodes % 1000 == 0 :
+                #    self.epsilon = self.epsilon * 0.9
                 pass
 
         obs = np.dot(obs, self.loadings)
@@ -66,7 +74,8 @@ class LinearBayesAgent:
     
     def init_Q(self):
         n = len(self.bucket)
-        return dict(zip(np.arange(0, n), np.ones((n, 2))))
+        np.random.seed(451)
+        return np.random.normal(0,1,(n,2))
 
     def get_Q(self, state, action):
         # Project states vector onto loadings subspace
@@ -82,14 +91,15 @@ class LinearBayesAgent:
         pass
 
     def get_action(self, state, training=True):
-        rand_prob = np.random.random()
-        if training and rand_prob < self.epsilon:
-            # Select an action at random
-            return env.action_space.sample()
-        else:
-            # Return the action with maximum Q-value
-            _, q_idx = self.get_Q(state, 0)
-            return np.argmax(q_idx)
+        if training:
+            rand_prob = np.random.random()
+            if rand_prob < self.epsilon:
+                # Select an action at random
+                return env.action_space.sample()
+
+        # Return the action with maximum Q-value
+        _, q_idx = self.get_Q(state, 0)
+        return np.argmax(q_idx)
     
     def update_policy(self, state, state_next, action, reward):
         state_value, Q_idx = self.get_Q(state, action)
@@ -98,36 +108,46 @@ class LinearBayesAgent:
         value_next = np.max(self.Q[Q_idx])
         # Update state_value using bellman equation
         state_value += self.lr*(reward + (self.gamma * value_next) - state_value)
+        self.Q[Q_idx][action] = state_value
+    
+    def full_update(self, all_states, actions, total_reward):
+        for t, act in enumerate(actions):
+            obs = all_states[t]
+            state_value, Q_idx = self.get_Q(obs, act)
+            self.Q[Q_idx][act] += self.lr*(total_reward - state_value)
 
-def run_sim(agent, episodes=100, train=True, plot=False):
+
+def run_sim(agent, episodes=100, train=True, plot=False, viz=False):
     data = {"obs":[], "reward":[],"actions":[]}
     for ep in range(episodes):
-        obs, reward, actions = agent.play_episode(train=train)
+        obs, reward, actions = agent.play_episode(train=train,viz=viz)
         data["actions"].append(actions)
         data["reward"].append(reward)
         data["obs"].append(obs)
-        if ep % episodes//10 == 0:
-            print(reward)
+        if (ep % (episodes//10)) == 0:
+            avg_rew = np.mean(data["reward"][-episodes//10:])
+            print(round(avg_rew,2), round(agent.epsilon, 2))
     
     if plot:
         df = pd.DataFrame(data)
         df.plot()
         plt.title(f"Training={train}")
         plt.show()
-
-        
-    
+    return agent
 
 if __name__ == "__main__":
     agent = LinearBayesAgent()
     #import pdb; pdb.set_trace()
-    run_sim(agent, episodes=10000, plot=True)
+    agent = run_sim(agent, episodes=10000, plot=False)
     df = pd.DataFrame(agent.Q)
-    #import pdb; pdb.set_trace()
-    df.to_csv("Q_table.csv", index=False)
+    
+    df.to_csv("Q_table.csv", index=True)
+    print(df.describe())
     #plt.scatter(np.arange(100), df.values[0])
     #plt.scatter(np.arange(100), df.values[1])
     #plt.show()
-    run_sim(agent, episodes=100, train=False)
-    
+    Q_max = np.argmax(agent.Q, axis=1)
+    print(Q_max)
+    run_sim(agent, episodes=10, train=False, viz=True)
+
 
